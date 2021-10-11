e1b0ed7ef11f1cdc32e257f874a85a4f61dac02a
Dan Solovay <dsolovay@gmail.com>
2021-10-11 10:24:29 -0400

    Test of vercel link comments

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Just a developer keeping up with developments...</p>
+        <p>Test of preview link generation.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
